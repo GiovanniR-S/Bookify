d72c7e301e678198b3eb6ceaced2ea6fd277b146
Giovanni Roberts Automação <giovanni.roberts@formacerta.com.br>
2026-02-16 15:02:45 -0300

    Atualizando apartment

---
+++ Bookify.Domain/Apartments/Description.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Domain.Apartments;
+public record Description(string Value);
---
+++ Bookify.Domain/Apartments/Name.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Domain.Apartments;
+
+public record Name(string Value);
