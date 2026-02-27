6d129cd27cda87bb06a7a5d36f49826dec4521b9
Giovanni Roberts Automação <giovanni.roberts@formacerta.com.br>
2026-02-27 11:33:16 -0300

    Implementa eventos de domínio e entidade User
    Adiciona suporte a eventos de domínio na Entity base, interface IDomainEvent integrada ao MediatR, value objects (Email, FirstName, LastName) e a entidade User com evento UserCreateDomainEvent. Reflete padrão DDD e facilita lógica reativa no domínio.

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -5,8 +5,16 @@ using System.Text;
 namespace Bookify.Domain.Abstractions;
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
+
     public Entity(Guid id) => Id = id;
 
     public Guid Id { get; init; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents() => _domainEvents.ToList();
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
 
 }
---
+++ Bookify.Domain/Users/FirstName.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Domain.Users;
+
+public record FirstName(string Value);
